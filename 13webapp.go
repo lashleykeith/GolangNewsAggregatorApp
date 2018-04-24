0b91907a2a166cb71d7fdcbcc771ef8727134a9f
Keith Lashley <lashleykeith@gmail.com>
2018-04-24 21:03:21 +0900

    Add files via upload

---
+++ 13webapp.go
@@ -0,0 +1,33 @@
+package main 
+
+import(
+	"encoding/xml"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+type Sitemapindex struct {
+	Locations []string `xml:"sitemap>loc"`
+}
+
+type News struct {
+	Titles []string `xml:"url>news>title"`
+	Keywords []string `xml:"url>news>keywords"`
+	Locations []string `xml:"url>loc"`
+}
+
+func main() {
+	var s Sitemapindex
+	var n News
+	resp, _ := http.Get("https://www.washingtonpost.com/news-sitemap-index.xml")
+	bytes, _ := ioutil.ReadAll(resp.Body)
+	xml.Unmarshal(bytes, &s)
+	news_map := make(map[string]NewsMap)
+
+	for _, Location := range s.Locations {
+		resp, _ := http.Get(Location)
+		bytes, _ := ioutil.ReadAll(resp.Body)
+		xml.Unmarshal(bytes, &n)
+	}
+}
