954891bbe7415867ebdc683e19ca7a75912ab2c6
Keith Lashley <lashleykeith@gmail.com>
2018-05-24 11:12:33 +0900

    Add files via upload

---
+++ 18goroutine.go
@@ -0,0 +1,20 @@
+package main
+
+import(
+	"time"
+	"fmt"
+)
+
+func say(s string) {
+	for i := 0; i < 3; i++ {
+		fmt.Println(s)
+		time.Sleep(time.Millisecond*100)
+	}
+}
+
+func main(){
+	go say("Hey")
+	say("There")
+
+	time.Sleep(time.Second)
+}
